aa154e2f44f3b898f70e643011ac668cf5c7d962
Donald Jones <jones.donald@gmail.com>
2015-09-11 18:12:29 -0500

    Added grunt tasks for faster development.

---
+++ gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        browserify: {
+            dev: {
+                files: {
+                    'public/javascripts/index.js': 'public/js/main.js'
+                },
+                options: {
+                    transform: ['stringify'],
+                    browserifyOptions: {
+                        debug: true
+                    }
+                }
+            }
+        },
+
+        less: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    sourceMapFileInline: true
+                },
+                files: {
+                    'public/css/compiled.css': 'public/css/**/*.less'
+                }
+            }
+        },
+
+        watch: {
+            client: {
+                files: ['views/**/*.html', 'public/js/**/*.js', 'public/css/**/*.less'],
+                tasks: ['concurrent:client'],
+                options: {
+                    spawn: true,
+                    livereload: true
+                }
+            }
+        },
+
+        concurrent: {
+            client: {
+                tasks: ['browserify:dev', 'less:dev'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['concurrent:client', 'watch:client']);
+};
