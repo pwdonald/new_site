a5dd38d87cbe8f0ea174003bac52736f64a3b79b
Donald <jones.donald@gmail.com>
2015-09-10 22:34:23 -0500

    Added lookup api methods for messages.

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,14 @@ var express = require('express'),
     router = express.Router(),
     Message = require('../models/messagemodel');
 
+var isAuthorized = function(req, res, next) {
+    if (!req.user) {
+        return res.status(401).end();
+    }
+
+    next();
+};
+
 router.post('/message', function(req, res) {
     req.body.ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     Message.saveMessage(req.body, function(err, message) {
@@ -10,6 +18,35 @@ router.post('/message', function(req, res) {
         }
 
         res.status(201).end();
+    });
+});
+
+router.get('/message', isAuthorized, function(req, res) {
+    Message.find({}, function(err, messages) {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        if (!messages) {
+            return res.status(404).end();
+        }
+
+        res.status(200).json(messages);
+    });
+});
+
+router.get('/message/:id', isAuthorized, function(req, res) {
+    var id = req.params.id || 0;
+    Message.findById(id, function(err, message) {
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        if (!message) {
+            return res.status(404).end();
+        }
+
+        res.status(200).json(message);
     });
 });
 
