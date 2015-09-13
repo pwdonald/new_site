5c6d39e902daa52a4032bce2f6d9e2ff6ca519d2
Donald <jones.donald@gmail.com>
2015-09-13 18:13:48 -0500

    Added notifications area.

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,29 @@
 var $ = window.jQuery = window.$ = require('jquery');
 var bootstrap = window.bootstrap = require('bootstrap');
+
+var notifications;
+
+$(document).ready(function() {
+    notifications = $('#notifications');
+
+    notifications.hide();
+});
+
+var submitContactMessage = function() {
+
+};
+
+window.displayNotification = function(message, alertType) {
+    notifications.removeClass('hidden');
+    notifications.children('#response')
+        .addClass(alertType)
+        .html(message);
+    notifications.show().fadeIn(500);
+};
+
+window.hideNotifications = function() {
+    notifications.children('#response')
+        .attr('class', 'alert');
+
+    notifications.hide().fadeOut(500);
+};
