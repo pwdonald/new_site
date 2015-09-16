8755efb44e3c1d242cd16f55ca1d8821daac2af8
Donald <jones.donald@gmail.com>
2015-09-15 19:20:09 -0500

    fixes #1 database autoload not working, replaced with explicit calls.

--- models/usermodel.js
+++ models/usermodel.js
@@ -2,39 +2,48 @@ var Datastore = require('nedb'),
     bcrypt = require('bcrypt');
 
 var User = new Datastore({
-    filename: 'data/users',
-    autoload: true
+    filename: 'data/users'
 });
 
 exports.findById = function(id, callback) {
-    User.findOne({
-        _id: id
-    }, callback);
+    User.loadDatabase(function(err) {
+        if (err) {
+            return err;
+        }
+
+        User.findOne({
+            _id: id
+        }, callback);
+    });
 };
 
 exports.findByUsername = function(username, callback) {
-    User.findOne({
-        username: username
-    }, function(err, user) {
-        callback(err, user);
+    User.loadDatabase(function(err) {
+        User.findOne({
+            username: username
+        }, function(err, user) {
+            callback(err, user);
+        });
     });
 };
 
 exports.isUsernameAvailable = function(req, res, next) {
-    User.findOne({
-        username: req.body.username
-    }, function(err, user) {
-        if (err) {
-            return next(err);
-        }
+    User.loadDatabase(function(err) {
+        User.findOne({
+            username: req.body.username
+        }, function(err, user) {
+            if (err) {
+                return next(err);
+            }
 
-        if (user) {
-            // not available
-            req.flash('error', 'Username invalid.');
-            return res.redirect('/register');
-        }
+            if (user) {
+                // not available
+                req.flash('error', 'Username invalid.');
+                return res.redirect('/register');
+            }
 
-        next();
+            next();
+        });
     });
 };
 
@@ -52,65 +61,71 @@ exports.checkPassword = function(user, password, callback) {
 };
 
 exports.createNewUser = function(req, res, next) {
-    User.insert({
-        username: req.body.username,
-        password: req.body.hashedPassword,
-        role: 0,
-        profile: {
-            fullName: '',
-            alias: '',
-            publicEmail: '',
-            location: '',
-            avatarUrl: '',
-            bio: ''
-        },
-        timestamp: new Date()
-    }, function(err, user) {
-        if (err) {
-            next(err);
-        }
+    User.loadDatabase(function(err) {
+        User.insert({
+            username: req.body.username,
+            password: req.body.hashedPassword,
+            role: 0,
+            profile: {
+                fullName: '',
+                alias: '',
+                publicEmail: '',
+                location: '',
+                avatarUrl: '',
+                bio: ''
+            },
+            timestamp: new Date()
+        }, function(err, user) {
+            if (err) {
+                next(err);
+            }
 
-        req.body.username = user.username;
+            req.body.username = user.username;
 
-        next();
+            next();
+        });
     });
 };
 
 exports.getUserProfile = function(req, res, next) {
-    User.findOne({
-        _id: req.user._id
-    }, function(err, user) {
-        if (err) {
-            next(err);
-        }
+    User.loadDatabase(function(err) {
+        User.findOne({
+            _id: req.user._id
+        }, function(err, user) {
+            if (err) {
+                next(err);
+            }
 
-        if (!user) {
-            res.status(500).end();
-            return;
-        }
+            if (!user) {
+                res.status(500).end();
+                return;
+            }
 
-        req.body.profile = user.profile || {};
+            req.body.profile = user.profile || {};
 
-        next();
+            next();
+        });
     });
 };
 
 exports.updateUserProfile = function(req, res, next) {
-    User.update({
-        _id: req.user._id
-    }, {
-        $set: {
-            profile: req.body
-        }
-    }, function(err, num) {
-        if (err) {
-            next(err);
-        }
+    User.loadDatabase(function(err) {
+        User.update({
+            _id: req.user._id
+        }, {
+            $set: {
+                profile: req.body
+            }
+        }, function(err, num) {
+            if (err) {
+                next(err);
+            }
 
-        if (num < 0) {
-            res.status(500).end();
-        }
+            if (num < 0) {
+                res.status(500).end();
+            }
 
-        next();
+            next();
+        });
     });
 };
